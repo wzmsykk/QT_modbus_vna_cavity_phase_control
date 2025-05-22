74a77c938606b66679577fdef2a3a690c86833c5
wzmsykk <wzmsykk@gmail.com>
2025-05-22 12:23:48 +0800

    First Exe Passed

--- pack.bat
+++ pack.bat
@@ -1,2 +1,3 @@
-nuitka --jobs=8 --msvc=latest --onefile --standalone --enable-plugin=pyqt5,implicit-imports --follow-imports  --include-plugin-directory=imports --include-data-files=data/CONVERTF.EXE=data/CONVERTF.EXE  main.py 
+set PYTHONPATH=.\imports;%PYTHONPATH%
+nuitka --jobs=8 --msvc=latest --onefile --standalone --enable-plugin=pyqt5,implicit-imports --follow-imports --include-plugin-directory=imports --include-data-files=data/CONVERTF.EXE=data/CONVERTF.EXE  main.py 
 pause
